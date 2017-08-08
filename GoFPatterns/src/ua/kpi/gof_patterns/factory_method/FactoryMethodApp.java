71c919454d64893486ae649a507594e2596b67a9
AlexKord <oleg.a.cherednichenko@gmail.com>
2017-08-08 15:27:58 +0300

    Facade & Factory Method patterns

---
+++ GoFPatterns/src/ua/kpi/gof_patterns/factory_method/FactoryMethodApp.java
@@ -0,0 +1,63 @@
+package ua.kpi.gof_patterns.factory_method;
+
+import java.util.Date;
+
+public class FactoryMethodApp {
+
+    public static WatchMaker getMakerByName(String maker) {
+        if (maker.equals("Digital")) {
+            return new DigitalWatchMaker();
+        } else if (maker.equals("Rome")) {
+            return new RomeWatchMaker();
+        } else {
+            throw new RuntimeException("Не поддерживаемая производственная линия часов: " + maker);
+        }
+    }
+
+    public static void main(String[] args) {
+        WatchMaker maker = getMakerByName("dsdsds");
+
+        Watch watch = maker.createWatch();
+        watch.showTime();
+    }
+}
+
+interface Watch {
+    void showTime();
+}
+
+class DigitalWatch implements Watch {
+
+    @Override
+    public void showTime() {
+        System.out.println(new Date());
+    }
+}
+
+class RomeWatch implements Watch {
+
+    @Override
+    public void showTime() {
+        System.out.println("VII-XX");
+    }
+}
+
+interface WatchMaker {
+    Watch createWatch();
+}
+
+class DigitalWatchMaker implements WatchMaker {
+
+    @Override
+    public Watch createWatch() {
+        return new DigitalWatch();
+    }
+}
+
+class RomeWatchMaker implements WatchMaker {
+
+    @Override
+    public Watch createWatch() {
+        return new RomeWatch();
+    }
+}
