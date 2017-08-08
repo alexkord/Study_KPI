badb9ffbe7ba91dd60524ffba7af21a025841b5c
AlexKord <oleg.a.cherednichenko@gmail.com>
2017-08-08 17:04:45 +0300

    State & Strategy patterns

--- GoFPatterns/src/ua/kpi/gof_patterns/strategy/StrategyApp.java
+++ GoFPatterns/src/ua/kpi/gof_patterns/strategy/StrategyApp.java
@@ -6,15 +6,15 @@ public class StrategyApp {
     public static void main(String[] args) {
         StrategyClient c = new StrategyClient();
 
-        int[] arr0 = {1,3,2,1};
+        int[] arr0 = {1, 3, 2, 1};
         c.setStrategy(new SelectionSort());
         c.executeStrategy(arr0);
 
-        int[] arr1 = {11,4,2,7,8,54};
+        int[] arr1 = {11, 4, 2, 7, 8, 54};
         c.setStrategy(new InsertingSort());
         c.executeStrategy(arr1);
 
-        int[] arr2 = {3,-8,2,0,33,1,3,2};
+        int[] arr2 = {3, -8, 2, 0, 33, 1, 3, 2};
         c.setStrategy(new BubbleSort());
         c.executeStrategy(arr2);
     }
