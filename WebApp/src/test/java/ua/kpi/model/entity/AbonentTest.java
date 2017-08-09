c18aa5118dd2fc4ff90663839fa1ee80e0c923c4
Alex Kord <oleg.a.cherednichenko@gmail.com>
2017-08-09 23:05:54 +0300

    some test

---
+++ WebApp/src/test/java/ua/kpi/model/entity/AbonentTest.java
@@ -0,0 +1,16 @@
+package ua.kpi.model.entity;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbonentTest {
+    @Test
+    public void createAbonent() throws Exception {
+        Abonent abonent1 = Abonent.createAbonent("Alex", "Kord");
+        Abonent abonent2 = abonent1;
+        assertEquals(abonent2, abonent1);
+    }
+
+}
