6a40896d027497be6258e1d3d23657f2b1adc956
alexkord <oleg.a.cherednichenko@gmail.com>
2017-08-15 18:01:07 +0300

    new task_3 @ patterns

--- pattern_tasks/src/ua/kpi/task_3/Tour.java
+++ pattern_tasks/src/ua/kpi/task_3/Tour.java
@@ -2,6 +2,32 @@ package ua.kpi.task_3;
 
 public class Tour {
     private String direction;
+    private boolean transfer;
+    private boolean fligth;
+    private boolean bookingHotel;
+    private boolean insurance;
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public boolean isTransfer() {
+        return transfer;
+    }
+
+    public boolean isFligth() {
+        return fligth;
+    }
+
+    public boolean isBookingHotel() {
+        return bookingHotel;
+    }
+
+    public boolean isInsurance() {
+        return insurance;
+    }
+
+
 
     public static class Builder {
 
