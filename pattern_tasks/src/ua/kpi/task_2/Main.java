0ec0919e631e9d7593ac8c965c66a8af31a18922
alexkord <oleg.a.cherednichenko@gmail.com>
2017-08-15 17:52:01 +0300

    new task_3 @ patterns

--- pattern_tasks/src/ua/kpi/task_2/Main.java
+++ pattern_tasks/src/ua/kpi/task_2/Main.java
@@ -3,8 +3,8 @@ package ua.kpi.task_2;
 public class Main {
     public static void main(String[] args) throws IllegalAccessException, InstantiationException {
         ManInAction manInAction =  new ManInAction();
-        AnimalViewer animalViewer = new AnimalViewer();
-        animalViewer.doAction(manInAction);
+        manInAction.setState(new RiverShore());
+        manInAction.changeState(new AnimalViewer());
         System.out.println(manInAction.getState());
     }
 }
---
+++ pattern_tasks/src/ua/kpi/task_3/Tour.java
@@ -0,0 +1,9 @@
+package ua.kpi.task_3;
+
+public class Tour {
+    private String direction;
+
+    public static class Builder {
+
+    }
+}
