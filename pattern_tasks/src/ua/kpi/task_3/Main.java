8fa5de6f2bf010695cd224b2e314dc62201aee34
alexkord <oleg.a.cherednichenko@gmail.com>
2017-08-16 12:47:25 +0300

    new task_3

---
+++ pattern_tasks/src/ua/kpi/task_3/Main.java
@@ -0,0 +1,16 @@
+package ua.kpi.task_3;
+
+public class Main {
+    public static void main(String[] args) {
+        Tour tour = new Tour.TourBuilder("New York")
+                .transferToAirport(false)
+                .transferfromAirport(true)
+                .flightThere(true)
+                .flightBack(true)
+                .bookingHotel(false)
+                .insurance(true)
+                .build();
+
+        System.out.println(tour);
+    }
+}
--- pattern_tasks/src/ua/kpi/task_3/Tour.java
+++ pattern_tasks/src/ua/kpi/task_3/Tour.java
@@ -2,34 +2,110 @@ package ua.kpi.task_3;
 
 public class Tour {
     private String direction;
-    private boolean transfer;
-    private boolean fligth;
-    private boolean bookingHotel;
-    private boolean insurance;
+    private boolean hasTransferToAirport;
+    private boolean hasTransferFromAirport;
+    private boolean hasFligthThere;
+    private boolean hasFligthBack;
+    private boolean isBookingHotel;
+    private boolean hasInsurance;
 
     public String getDirection() {
         return direction;
     }
 
-    public boolean isTransfer() {
-        return transfer;
+    public boolean isHasTransferToAirport() {
+        return hasTransferToAirport;
     }
 
-    public boolean isFligth() {
-        return fligth;
+    public boolean isHasTransferFromAirport() {
+        return hasTransferFromAirport;
+    }
+
+    public boolean isHasFligthThere() {
+        return hasFligthThere;
+    }
+
+    public boolean isHasFligthBack() {
+        return hasFligthBack;
     }
 
     public boolean isBookingHotel() {
-        return bookingHotel;
+        return isBookingHotel;
     }
 
-    public boolean isInsurance() {
-        return insurance;
+    public boolean isHasInsurance() {
+        return hasInsurance;
     }
 
+    private Tour(TourBuilder builder) {
+        this.direction = builder.direction;
+        this.hasTransferToAirport = builder.hasTransferToAirport;
+        this.hasTransferFromAirport = builder.hasTransferFromAirport;
+        this.hasFligthThere = builder.hasFligthThere;
+        this.hasFligthBack = builder.hasFligthBack;
+        this.isBookingHotel = builder.isBookingHotel;
+        this.hasInsurance = builder.hasInsurance;
+    }
 
+    public static class TourBuilder {
+        private String direction;
+        private boolean hasTransferToAirport;
+        private boolean hasTransferFromAirport;
+        private boolean hasFligthThere;
+        private boolean hasFligthBack;
+        private boolean isBookingHotel;
+        private boolean hasInsurance;
 
-    public static class Builder {
+        public TourBuilder(String direction) {
+            this.direction = direction;
+        }
 
+        public TourBuilder transferToAirport(boolean hasTransferToAirport) {
+            this.hasTransferToAirport = hasTransferToAirport;
+            return this;
+        }
+
+        public TourBuilder transferfromAirport(boolean hasTransferFromAirport) {
+            this.hasTransferFromAirport = hasTransferFromAirport;
+            return this;
+        }
+
+        public TourBuilder flightThere(boolean hasFligthThere) {
+            this.hasFligthThere = hasFligthThere;
+            return this;
+        }
+
+        public TourBuilder flightBack(boolean hasFligthBack) {
+            this.hasFligthBack = hasFligthBack;
+            return this;
+        }
+
+        public TourBuilder bookingHotel(boolean isBookingHotel) {
+            this.isBookingHotel = isBookingHotel;
+            return this;
+        }
+
+        public TourBuilder insurance(boolean hasInsurance) {
+            this.hasInsurance = hasInsurance;
+            return this;
+        }
+
+        Tour build() {
+            return new Tour(this);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Tour{" +
+                "direction='" + direction + '\'' +
+                ", hasTransferToAirport=" + hasTransferToAirport +
+                ", hasTransferFromAirport=" + hasTransferFromAirport +
+                ", hasFligthThere=" + hasFligthThere +
+                ", hasFligthBack=" + hasFligthBack +
+                ", isBookingHotel=" + isBookingHotel +
+                ", hasInsurance=" + hasInsurance +
+                '}';
     }
 }
