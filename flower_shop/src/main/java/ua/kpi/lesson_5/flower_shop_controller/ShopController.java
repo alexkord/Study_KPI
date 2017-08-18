23b81a66253c1a3f1cce2a54d3230dd84e0baef0
Alex Kord <oleg.a.cherednichenko@gmail.com>
2017-08-18 17:33:45 +0300

    ...

--- flower_shop/src/main/java/ua/kpi/lesson_5/flower_shop_controller/ShopController.java
+++ flower_shop/src/main/java/ua/kpi/lesson_5/flower_shop_controller/ShopController.java
@@ -2,7 +2,10 @@ package ua.kpi.lesson_5.flower_shop_controller;
 
 import ua.kpi.lesson_5.flower_shop_model.ShopModel;
 import ua.kpi.lesson_5.flower_shop_model.entity.bouquet.Bouquet;
-import ua.kpi.lesson_5.flower_shop_model.entity.herb.*;
+import ua.kpi.lesson_5.flower_shop_model.entity.herb.Branch;
+import ua.kpi.lesson_5.flower_shop_model.entity.herb.Flower;
+import ua.kpi.lesson_5.flower_shop_model.entity.herb.Herb;
+import ua.kpi.lesson_5.flower_shop_model.entity.herb.NotAFlower;
 import ua.kpi.lesson_5.view_shop.ViewFlowerShop;
 
 import java.util.ArrayList;
@@ -19,48 +22,29 @@ public class ShopController {
     }
 
     public void start() {
-        shopModel.setBouquet(createBouquet());
-        ViewFlowerShop.print(shopModel.getBouquet().toString());
-    }
-
-    private Bouquet createBouquet() {
-        return new Bouquet(getHerbs());
-    }
-
-    private List<Herb> getHerbs() {
         Scanner scanner = new Scanner(System.in);
+        createBouquet(scanner);
+    }
+
+    private Bouquet createBouquet(Scanner scanner) {
+        Bouquet bouquet = new Bouquet(getHerbs(scanner));
+        return bouquet;
+    }
+
+    private List<Herb> getHerbs(Scanner scanner) {
+        viewFlowerShop.print(ViewFlowerShop.FIRST_MESSAGE);
         List<Herb> herbs = new ArrayList<>();
-        int i = 0;
-        while (i != ShopModel.COUNT_OF_HERBS) {
-            try {
-                herbs.add(getHerbFactory(scanner));
-            } catch (FlowerShopException e) {
-                e.printStackTrace();
-            }
-            i++;
-        }
+        Branch b = new Branch("Branch");
+        Flower f = new Flower("Rose");
+        NotAFlower naf = new NotAFlower("Not a flower");
+        herbs.add(b);
+        herbs.add(f);
+        herbs.add(naf);
         return herbs;
     }
 
-    private Herb getHerbFactory(Scanner scanner) throws FlowerShopException {
-        ViewFlowerShop.print(ViewFlowerShop.TYPE_OF_HERB);
-        String herbType = scanner.next();
-        ViewFlowerShop.print(ViewFlowerShop.NAME_OF_HERB);
-        String herbName = scanner.next();
-        if (herbType.equalsIgnoreCase("Branch")) {
-            return new Branch(herbName);
-        } else if (herbType.equalsIgnoreCase("Flower")) {
-            return new Flower(herbName);
-        } else if (herbType.equalsIgnoreCase("Multiply Blossom")) {
-            return new MultiplyBlossomFlower(herbName);
-        } else if (herbType.equalsIgnoreCase("Not a flower")) {
-            return new NotAFlower(herbName);
-        } else if (herbType.equalsIgnoreCase("Shrub")) {
-            return new Shrub(herbName);
-        } else if (herbType.equalsIgnoreCase("Single Blossom")) {
-            return new SingleBlossomFlower(herbName);
-        } else {
-            throw new FlowerShopException("Enter correct herb");
-        }
+    private String getChoice() {
+        return ViewFlowerShop.MULTIPLY_BLOSSOM;
     }
+
 }
--- flower_shop/src/main/java/ua/kpi/lesson_5/view_shop/ViewFlowerShop.java
+++ flower_shop/src/main/java/ua/kpi/lesson_5/view_shop/ViewFlowerShop.java
@@ -1,12 +1,16 @@
 package ua.kpi.lesson_5.view_shop;
 
 public class ViewFlowerShop {
-    public static final String TYPE_OF_HERB = "Enter type of herb that u want to add: ";
-    public static final String NAME_OF_HERB = "Enter name of herb:  ";
 
+    public static final String BRANCH = "What would u like to create: ";
+    public static final String FLOWER = "What would u like to create: ";
+    public static final String MULTIPLY_BLOSSOM = "What would u like to create: ";
+    public static final String MESSAGE = "What would u like to create: ";
+    //input
+    public static final String FIRST_MESSAGE = "What would u like to create? ";
     public static void print(String... messages) {
         for (String message: messages) {
-            System.out.print(message);
+            System.out.println(message);
         }
     }
 }
