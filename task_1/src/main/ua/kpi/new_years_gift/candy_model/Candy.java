c231086af67edb9915ecf6cafb4ad7c3e74d3205
Alex Kord <oleg.a.cherednichenko@gmail.com>
2017-08-20 12:09:24 +0300

    ...

---
+++ task_1/src/main/ua/kpi/new_years_gift/candy_model/Candy.java
@@ -0,0 +1,5 @@
+package main.ua.kpi.new_years_gift.candy_model;
+
+public abstract class Candy implements Item {
+
+}
---
+++ task_1/src/main/ua/kpi/new_years_gift/candy_model/Item.java
@@ -0,0 +1,7 @@
+package main.ua.kpi.new_years_gift.candy_model;
+
+public interface Item {
+    String name();
+    double sugarContent();
+    int weight();
+}
