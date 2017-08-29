0c4ee6998056e786646d20f6bd7b50722be235f7
alexkord <oleg.a.cherednichenko@gmail.com>
2017-08-29 16:13:14 +0300

    finish task1

---
+++ task_1/src/main/ua/kpi/new_years_gift/util/FindBySugarRange.java
@@ -0,0 +1,21 @@
+package main.ua.kpi.new_years_gift.util;
+
+import main.ua.kpi.new_years_gift.model.candy_model.Item;
+import main.ua.kpi.new_years_gift.model.gift_model.Gift;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FindBySugarRange {
+
+    public List<Item> findSugarRange(Gift gift, int from, int to) {
+        List<Item> items = new ArrayList<>();
+        for (Item item: gift.getItems()) {
+            if (item.sugarContent() >= from && item.sugarContent() <= to) {
+                items.add(item);
+            }
+        }
+        return items;
+    }
+}
